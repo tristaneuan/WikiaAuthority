d6ab75bc6aeaaf55b4c5c6150ea12215d10579c0
Tristan Chong <tristanchong@gmail.com>
2014-05-15 21:44:08 +0000

    write test_get_all_revisions

--- tests/api_to_database_test.py
+++ tests/api_to_database_test.py
@@ -1,4 +1,3 @@
-import pytest
 from moto import mock_s3
 
 from .. import api_to_database as atd
@@ -13,6 +12,23 @@ def test_get_all_titles():
     assert u'ns' in title
     assert u'pageid' in title
     assert u'title' in title
+
+
+def test_get_all_revisions():
+    atd.api_url = 'http://muppet.wikia.com/api.php'
+    titles = atd.get_all_titles()
+    title = titles[0]
+    all_revisions = atd.get_all_revisions(title)
+    assert isinstance(all_revisions, list)
+    title_string, revisions = all_revisions
+    assert isinstance(title_string, unicode)
+    assert isinstance(revisions, list)
+    revision = revisions[0]
+    assert isinstance(revision, dict)
+    assert u'user' in revision
+    assert u'userid' in revision
+    assert u'revid' in revision
+    assert u'parentid' in revision
 
 
 @mock_s3
