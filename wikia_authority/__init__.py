9197415dab12b10b9dbed38bf374396ac14a4515
Tristan Chong <tristanchong@gmail.com>
2014-05-21 20:49:07 +0000

    make denominator 1.0 if 0

--- wikia_authority/__init__.py
+++ wikia_authority/__init__.py
@@ -8,10 +8,17 @@ class MinMaxScaler:
         self.max = max(vals)
         self.enforced_min = enforced_min
         self.enforced_max = enforced_max
+        print 'min', str(self.min)
+        print 'max', str(self.max)
+        print 'enforced_min', str(self.enforced_min)
+        print 'enforced_max', str(self.enforced_max)
 
     def scale(self, val):
-        return (((self.enforced_max - self.enforced_min) * (val - self.min))
-                / (self.max - self.min)) + self.enforced_min
+        denominator = self.max - self.min
+        if denominator == 0:
+            denominator = 1.0
+        return (((self.enforced_max - self.enforced_min) * (val - self.min)) /
+                denominator) + self.enforced_min
 
 
 def log(*args):
